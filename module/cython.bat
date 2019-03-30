c1fa481db5d7fa25003699b41fd3970b04926c6e
mgambrell <>
2019-03-30 09:22:16 -0500

    refine python building scripts

--- module/cython.bat
+++ module/cython.bat
@@ -5,7 +5,10 @@ pushd %~dp0
 set BASEDIR=%~dp0\..\..\
 
 set PYDIR=%BASEDIR%python27\
-set path=%PYDIR%;%PATH%
+set path=%PYDIR%;%PYDIR%Scripts;%PATH%
+
+rem sigh..... @#&*(@#ing python.
+mklink /D c:\python27 %PYDIR%
 
 python setup.py build_ext %*
 
