d91db54fd7db52b9602ed449b7351ec810c7d7c1
mgambrell <>
2019-03-31 16:44:50 -0500

    improve cython build

--- module/cython.bat
+++ module/cython.bat
@@ -8,6 +8,7 @@ set PYDIR=%BASEDIR%python27-for-tools\
 set path=%PYDIR%;%PYDIR%Scripts;%PATH%
 
 rem sigh..... @#&*(@#ing python.
+rmdir c:\python27
 mklink /D c:\python27 %PYDIR%
 
 python setup.py build_ext %*
