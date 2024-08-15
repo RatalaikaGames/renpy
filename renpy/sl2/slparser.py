a3c2c144ee6013049815d46cee0a1741a7616a31
ratalaika <ratalaika@ratalaika.com>
2024-08-15 17:50:58 +0200

    remove typing imports

--- renpy/sl2/slparser.py
+++ renpy/sl2/slparser.py
@@ -21,7 +21,7 @@
 
 from __future__ import division, absolute_import, with_statement, print_function, unicode_literals
 from renpy.compat import PY2, basestring, bchr, bord, chr, open, pystr, range, round, str, tobytes, unicode # *
-from typing import Any, Callable, Literal
+#from typing import Any, Callable, Literal
 
 
 import collections
