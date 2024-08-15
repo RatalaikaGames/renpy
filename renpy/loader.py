1c7ff08030afb08005fe2894a00d4241a5e402d1
David Amador <david.djlink@gmail.com>
2024-08-15 14:46:17 +0100

    removed use of RWops_from_file and Rwops_createsubfile

--- renpy/loader.py
+++ renpy/loader.py
@@ -34,7 +34,7 @@ import re
 import io
 import unicodedata
 
-from pygame_sdl2.rwobject import RWops_from_file, RWops_create_subfile
+#from pygame_sdl2.rwobject import RWops_from_file, RWops_create_subfile
 
 from renpy.compat.pickle import loads
 from renpy.webloader import DownloadNeeded
@@ -626,18 +626,19 @@ class SubFile(object):
         raise Exception("Write not supported by SubFile")
 
 
-open_file = RWops_from_file # type: ignore
+open_file = open # type: ignore
 
 if "RENPY_FORCE_SUBFILE" in os.environ:
 
     def open_file(name, mode):
-        f = RWops_from_file(name, mode)
+        f = open(name, mode)
 
         f.seek(0, 2)
         length = f.tell()
         f.seek(0, 0)
 
-        return RWops_create_subfile(f, 0, length)
+        return SubFile(f, 0, length, b'')
+        
 
 # A list of callbacks to open an open python file object of the given type.
 file_open_callbacks = [ ]
@@ -730,11 +731,12 @@ def load_from_archive(name):
             else:
                 offset, dlen, start = t
 
-            if start == None or len(start) == 0:
-                rw = RWops_from_file(afn, "rb")
-                rv = RWops_create_subfile(rw, offset, dlen)
-            else:
-                rv = SubFile(afn, offset, dlen, start)
+            rv = SubFile(afn, offset, dlen, start)
+            #if start == None or len(start) == 0:
+            #    rw = RWops_from_file(afn, "rb")
+            #    rv = RWops_create_subfile(rw, offset, dlen)
+            #else:
+            #    rv = SubFile(afn, offset, dlen, start)
 
         # Compatibility path.
         else:
