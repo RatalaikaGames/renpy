ecbd2ed1abe589a1d63a05fd2eb7fd03792c4854
David Amador <david.djlink@gmail.com>
2024-08-15 17:28:19 +0100

    changed to malloc back, was having some crash errors

--- module/renpysound_core.c
+++ module/renpysound_core.c
@@ -408,8 +408,8 @@ static void callback(void *userdata, Uint8 *stream, int length) {
 
     length /= 4;
 
-    float* mix_buffer = (float*)alloca(length * 2 * sizeof(float));
-    short* stream_buffer = (short*)alloca(length * 2 * sizeof(short));
+    float* mix_buffer = (float*)malloc(length * 2 * sizeof(float));
+    short* stream_buffer = (short*)malloc(length * 2 * sizeof(short));
 
     memset(mix_buffer, 0, length * 2 * sizeof(float));
 
